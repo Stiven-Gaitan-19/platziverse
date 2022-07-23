dc060c2846b720dded3a2cca9c058ce3b29ad39e
Stiven Gaitan <johan.gaitan@mqa-bc.com>
2022-07-22 21:21:51 -0500

    service metric was added

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -4,6 +4,7 @@ const setupDatabase = require('./lib/db')
 const setupAgentModel = require('./models/agent')
 const setupMetricModel = require('./models/metric')
 const setupAgent = require('./lib/agent')
+const setupMetric = require('./lib/metric')
 
 module.exports = async function (config) {
   const sequelize = setupDatabase(config)
@@ -20,7 +21,7 @@ module.exports = async function (config) {
   }
 
   const Agent = setupAgent(AgentModel)
-  const Metric = {}
+  const Metric = setupMetric(MetricModel)
 
   return {
     Agent,
---
+++ platziverse-db/lib/metric.js
@@ -0,0 +1,49 @@
+'use strict'
+
+module.exports = function setupMetric (agentModel, metricModel) {
+  async function findByUuid (uuid) {
+    return metricModel.findAll({
+      raw: true,
+      attributes: ['type'],
+      group: ['type'],
+      include: [
+        {
+          attributes: [],
+          model: agentModel,
+          where: { uuid }
+        }
+      ]
+    })
+  }
+
+  async function findByTypeAgentUuid (type, uuid) {
+    return metricModel.findAll({
+      where: { type },
+      limit: 20,
+      order: [['created', 'DESC']],
+      include: [
+        {
+          model: agentModel,
+          attributes: [],
+          where: { uuid }
+        }
+      ]
+    })
+  }
+
+  async function create (uuid, metric) {
+    const agent = await agentModel.findOne({ where: { uuid } })
+
+    if (agent) {
+      metric.agentId = agent.id
+      const result = await metricModel.create(metric)
+      return result.toJSON()
+    }
+  }
+
+  return {
+    findByUuid,
+    findByTypeAgentUuid,
+    create
+  }
+}
