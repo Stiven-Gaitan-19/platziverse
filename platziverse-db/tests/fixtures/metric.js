e7a0f4d597551e4ab011199a30ffde7392796e32
Stiven Gaitan <johan.gaitan@mqa-bc.com>
2022-07-25 21:15:37 -0500

    metric service test added

--- platziverse-db/tests/fixtures/metric.js
+++ platziverse-db/tests/fixtures/metric.js
@@ -1,17 +1,20 @@
 'use strict'
 
+const agentFixture = require('./agent')
+
 const metric = {
   id: 1,
   type: 'test',
   value: 'Hello this is for test',
-  agentId: 1
+  agentId: 1,
+  agent: agentFixture.getOne
 }
 
 const metrics = [
   metric,
-  cloneObject(metric, { id: 2, type: 'type1', value: 'random metric', agentId: 2 }),
+  cloneObject(metric, { id: 2, type: 'type1', value: 'random metric', agentId: 2, agent: agentFixture.getAll[2] }),
   cloneObject(metric, { id: 3, type: 'type2' }),
-  cloneObject(metric, { id: 4, type: 'type3', agentId: 3 })
+  cloneObject(metric, { id: 4, type: 'test', agentId: 3, agent: agentFixture.getAll[3] })
 ]
 
 function cloneObject (obj, values) {
@@ -19,7 +22,34 @@ function cloneObject (obj, values) {
   return Object.assign(clone, values)
 }
 
+function getAllByUuid(uuid){
+  return metrics.filter(metric => metric.agent.uuid === uuid)
+}
+
+function getOneByUuid(uuid){
+  return metrics.filter(metric => metric.agent.uuid === uuid)
+}
+
+function getAllByTypeAndUuid(type, uuid){
+  return metrics.filter(metric => {
+    let metType = metric.type
+    let metUuid = metric.agent.uuid
+    return (metType === type && metUuid === uuid)
+  })
+}
+
+function newMetric(){
+  let newMetric = {...metric};
+  delete newMetric.agent;
+  delete newMetric.agentId;
+  return newMetric;
+}
+
 module.exports = {
   metric,
-  metrics
+  metrics,
+  getAllByUuid,
+  getOneByUuid,
+  getAllByTypeAndUuid,
+  newMetric
 }
--- platziverse-db/tests/metric.test.js
+++ platziverse-db/tests/metric.test.js
@@ -4,11 +4,45 @@ const test = require('ava')
 const sinon = require('sinon')
 const proxyquire = require('proxyquire')
 const metricFixture = require('./fixtures/metric')
+const agentFixture = require('./fixtures/agent')
 
 let db = null
 let sandbox = null
 let MetricStub = null
 const uuid = 'yyy-yyy-yyy'
+const type = 'test'
+
+const AgentStub = {
+  hasMany: sinon.spy()
+}
+
+const uuidParams = {
+  raw: true,
+  attributes: ['type'],
+  group: ['type'],
+  include: [
+    {
+      attributes: [],
+      model: AgentStub,
+      where: { uuid }
+    }
+  ]
+}
+const typeAgentUuidParams = {
+  where: { type },
+  limit: 20,
+  order: [['created', 'DESC']],
+  include: [
+    {
+      model: AgentStub,
+      attributes: [],
+      where: { uuid }
+    }
+  ]
+}
+const whereUuid = {
+  where: { uuid }
+}
 
 const dbConfig = {
   dialect: 'sqlite',
@@ -23,19 +57,21 @@ const dbConfig = {
   logging: () => {}
 }
 
-const AgentStub = {
-  hasMany: sinon.spy()
-}
-
 test.beforeEach(async () => {
   sandbox = sinon.createSandbox()
 
   MetricStub = {
     belongsTo: sandbox.spy(),
-    findAll: sandbox.stub(),
     create: sandbox.stub(),
-    findOne: sandbox.stub()
+    findAll: sandbox.stub(),
   }
+
+  MetricStub.findAll.withArgs(uuidParams).returns(Promise.resolve(metricFixture.getAllByUuid))
+  MetricStub.findAll.withArgs(typeAgentUuidParams).returns(Promise.resolve(metricFixture.getAllByTypeAndUuid))
+  MetricStub.create.withArgs({...metricFixture.newMetric(), agentId: agentFixture.getOne.id}).returns(Promise.resolve({ toJSON() { return {...metricFixture.newMetric(), agentId: agentFixture.getOne.id, agent: agentFixture.getOne} }}))
+
+  AgentStub.findOne = sinon.stub()
+  AgentStub.findOne.withArgs(whereUuid).returns(Promise.resolve(agentFixture.getOne))
 
   const setupDataBase = proxyquire('../', {
     './models/agent': () => AgentStub,
@@ -59,44 +95,44 @@ test.serial('Setub', t => {
 })
 
 test.serial('Metric #findByUuid', async (t) => {
-  const metrics = await db.Metric.findByUuid(uiid)
+  const metrics = await db.Metric.findByUuid(uuid)
 
-  t.true(metricStub.findAll.called, 'findAll should be called')
-  t.true(metricStub.findAll.calledOne, 'findAll should be called only once')
-  t.true(metricStub.findAll.calledWith(), 'findOne should be called with the condition')
+  t.true(MetricStub.findAll.called, 'findAll should be called')
+  t.true(MetricStub.findAll.calledOnce, 'findAll should be called only once')
+  t.true(MetricStub.findAll.calledWith(uuidParams), 'findAll should be called with the condition')
 
-  t.deepEqual(metrics, metricFixture.getByUuid, 'should be the same object')
+  t.deepEqual(metrics, metricFixture.getAllByUuid, 'should be the same object')
 })
 
 test.serial('Metric #findByTypeAgentUuid', async (t) => {
-  const metrics = await db.Metric.findByUuid(uiid)
+  const metrics = await db.Metric.findByTypeAgentUuid(type, uuid)
 
-  t.true(metricStub.findAll.called, 'findAll should be called')
-  t.true(metricStub.findAll.calledOne, 'findAll should be called only once')
-  t.true(metricStub.findAll.calledWith(), 'findOne should be called with the condition')
+  t.true(MetricStub.findAll.called, 'findAll should be called')
+  t.true(MetricStub.findAll.calledOnce, 'findAll should be called only once')
+  t.true(MetricStub.findAll.calledWith(typeAgentUuidParams), 'findAll should be called with the condition')
 
-  t.deepEqual(metrics, metricFixture.getByUuid, 'should be the same object')
+  t.deepEqual(metrics, metricFixture.getAllByTypeAndUuid, 'should be the same object')
 })
 
-test.serial('Metric #create - with nonexists uuid', async (t) => {
-  const metrics = await db.Metric.findByUuid(uiid)
+test.serial('Metric #create - with nonexists agent', async (t) => {
+  const metric = await db.Metric.save('tttt-ppp-ddd', metricFixture.newMetric())
 
-  t.true(metricStub.findOne.called, 'findOne should be called')
-  t.true(metricStub.findOne.calledOne, 'findOne should be called only once')
-  t.true(metricStub.findOne.calledWith(), 'findOne should be called with the condition')
-
-  t.deepEqual(metrics, metricFixture.getByUuid, 'should be the same object')
+  t.true(AgentStub.findOne.called, 'findOne should be called')
+  t.true(AgentStub.findOne.calledOnce, 'findOne should be called only once')
+  t.false(AgentStub.findOne.calledWith(whereUuid), 'findOne should be called with the condition')
+  t.deepEqual(metric, undefined, 'should not be present')
 })
 
-test.serial('Metric #create - with existing uuid', async (t) => {
-  const metrics = await db.Metric.findByUuid(uiid)
+test.serial('Metric #create - with existing agent', async (t) => {
+  const metric = await db.Metric.save(uuid, metricFixture.newMetric())
 
-  t.true(metricStub.findOne.called, 'findOne should be called')
-  t.true(metricStub.findOne.calledOne, 'findOne should be called only once')
-  t.true(metricStub.findOne.calledWith(), 'findOne should be called with the condition')
-  t.true(metricStub.create.called, 'create should be called')
-  t.true(metricStub.create.calledOne, 'create should be called only once')
-  t.true(metricStub.create.calledWith(), 'findOne should be called with the condition')
+  t.true(AgentStub.findOne.called, 'findOne should be called')
+  t.true(AgentStub.findOne.calledOnce, 'findOne should be called only once')
+  t.true(AgentStub.findOne.calledWith(whereUuid), 'findOne should be called with the condition')
 
-  t.deepEqual(metrics, metricFixture.getByUuid, 'should be the same object')
+  t.true(MetricStub.create.called, 'create should be called')
+  t.true(MetricStub.create.calledOnce, 'create should be called only once')
+  t.true(MetricStub.create.calledWith({...metricFixture.newMetric(), agentId: agentFixture.getOne.id}), 'create should be called with the condition')
+
+  t.deepEqual(metric, {...metricFixture.newMetric(), agentId: agentFixture.getOne.id, agent: agentFixture.getOne}, 'should be the same object')
 })
